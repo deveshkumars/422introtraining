cef4c8fe5b2c1533c349b7a9370704e345ff5bb6
vzceti <53797827+vzceti@users.noreply.github.com>
2021-03-21 14:30:25 -0400

    Update Autonomous.java

--- src/main/java/frc/robot/commands/autonomous/Autonomous.java
+++ src/main/java/frc/robot/commands/autonomous/Autonomous.java
@@ -1,15 +1,12 @@
 package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-// import edu.wpi.first.wpilibj2.command.WaitCommand;
-// import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import frc.robot.commands.*;
 
-// The real file
 public class Autonomous extends SequentialCommandGroup {
 
     public enum PathNames {
-        BARREL, SLALOM, BOUNCE, CIRCUIT
+        BARREL, SLALOM, BOUNCE
     }
 
     public static PathNames pathName;
@@ -17,13 +14,13 @@ public class Autonomous extends SequentialCommandGroup {
     public Autonomous(){
         pathName = PathNames.BARREL;
         if (pathName == PathNames.BARREL) {
-            addCommands(new DriveStraight(106.5, 0.6));
-            addCommands(new Circle(18, 356.65));
-            addCommands(new DriveStraight(83, 0.6));
-            addCommands(new Circle(-18, -287));
+            addCommands(new DriveStraight(76.5, 0.6));
+            addCommands(new Circle(18, 360.65));
+            addCommands(new DriveStraight((83-18), 0.6));
+            addCommands(new Circle(-18, -305));
             addCommands(new DriveStraight(73, 0.6));
             addCommands(new Circle(-18, -225));
-            addCommands(new DriveStraight(260,0.6)); //260 is a placeholder
+            addCommands(new DriveStraight(230,0.6)); //260 is a placeholder
         }
     }
 }
