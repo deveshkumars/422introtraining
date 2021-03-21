3f2ef048daa39887685ee8262f7c0064c5fdbbc8
WAaron-500 <61605186+WAaron-500@users.noreply.github.com>
2021-03-21 14:22:27 -0400

    Update Autonomous.java

--- src/main/java/frc/robot/commands/autonomous/Autonomous.java
+++ src/main/java/frc/robot/commands/autonomous/Autonomous.java
@@ -1,14 +1,29 @@
 package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+// import edu.wpi.first.wpilibj2.command.WaitCommand;
+// import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import frc.robot.commands.*;
 
 // The real file
-public final class Autonomous {
+public class Autonomous extends SequentialCommandGroup {
 
-    public void initialize() {
-
+    public enum PathNames {
+        BARREL, SLALOM, BOUNCE, CIRCUIT
     }
-    
+
+    public static PathNames pathName;
+
+    public Autonomous(){
+        pathName = PathNames.BARREL;
+        if (pathName == PathNames.BARREL) {
+            addCommands(new DriveStraight(106.5, 0.6));
+            addCommands(new Circle(18, 356.65));
+            addCommands(new DriveStraight(83, 0.6));
+            addCommands(new Circle(-18, -287));
+            addCommands(new DriveStraight(73, 0.6));
+            addCommands(new Circle(-18, -225));
+            addCommands(new DriveStraight(260,0.6)); //260 is a placeholder
+        }
+    }
 }
