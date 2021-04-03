6d8a39dc60091618016491d01142ac8d1fac43c1
vzceti <53797827+vzceti@users.noreply.github.com>
2021-04-03 16:28:34 -0400

    Update Autonomous.java

--- src/main/java/frc/robot/commands/autonomous/Autonomous.java
+++ src/main/java/frc/robot/commands/autonomous/Autonomous.java
@@ -10,7 +10,7 @@ public class Autonomous extends SequentialCommandGroup {
     }
 
     /** The path to use if an error occurs or none is selected. */
-    public static Path defaultAuto = Path.BARREL;
+    public static Path defaultAuto = Path.BOUNCE;
 
     /** The path used on this specific auto object. */
     public Path path;
@@ -18,65 +18,42 @@ public class Autonomous extends SequentialCommandGroup {
     public Autonomous(Path path) {
         this.path = path;
         switch (path) {
-            case BARREL: //center halfway between B2 and D2
+            case BARREL: //robot's front center is halfway between B2 and D2
                 addCommands(new DriveStraight(76.5, 0.6));
-                addCommands(new Circle(18, 360.65));
-                addCommands(new DriveStraight((83-18), 0.6));
-                addCommands(new Circle(-18, -305));
-                addCommands(new DriveStraight(73, 0.6));
-                addCommands(new Circle(-18, -225)); //this boi needs to be tweaked a little
-                addCommands(new DriveStraight(230,0.6)); 
+                addCommands(new Circle(18, 355));
+                addCommands(new DriveStraight((83-15), 0.6)); //change
+                addCommands(new Circle(-18, -308));
+                addCommands(new DriveStraight(36, 0.6));
+                addCommands(new Circle(-18, -218)); 
+                addCommands(new DriveStraight(240,0.6)); 
                 break;
-            case SLALOM: //center 5.5 inches above E2
-                // addCommands(new Turn(-37, 0.4)); //turn to line up with first gap
-                // addCommands(new DriveStraight(91, 0.4)); //drive through first gap
-                // addCommands(new Turn(37, 0.4)); //turn to be parallel with line of cones
-                // addCommands(new DriveStraight(100, 0.4)); //drive to end of line of cones
-                // addCommands(new Turn(37, 0.4)); //turn to line up with second gap
-                // addCommands(new DriveStraight(33.98, 0.4)); //drive to tangent point with circle around last cone
-                // addCommands(new Turn(55, 0.4));
-                // addCommands(new Circle(-30, 360)); //drive circle around last cone
-                // addCommands(new DriveStraight(33.98, 0.4)); //drive from circle to start of line of cones
-                // addCommands(new Turn (55, 0.4)); //turn to be parallel with line of cones
-                // addCommands(new Turn (37, 0.4));
-                // addCommands(new DriveStraight(100, 0.4)); //drive to end of line of cones
-                // addCommands(new Turn(37, 0.4)); //turn to line up with end position
-                // addCommands(new DriveStraight(90, 0.4)); //drive to end position
-
-                addCommands(new DriveStraight(16.5, 0.6));
-                addCommands(new Circle(-30, 90));
-                addCommands(new Circle(30, 90));
-                addCommands(new DriveStraight(120, 0.4)); //drive to end of line of cones
-                addCommands(new Circle(30, 90));
-                addCommands(new Circle(-30, 360));
-                addCommands(new Circle(30, 90));
-                addCommands(new DriveStraight(120, 0.4)); //drive to end of line of cones
-                addCommands(new Circle(30, 90));
-                addCommands(new Circle(-30, 90));
+            case SLALOM: 
+                //left front wheel is 8.5 inches from D2
+                addCommands(new DriveStraight(13,0.4));
+                addCommands(new Turn(-50, 0.4)); //turn to line up with first gap
+                addCommands(new DriveStraight(91, 0.4)); //drive through first gap
+                addCommands(new Turn(55, 0.4)); //turn to be parallel with line of cones
+                addCommands(new DriveStraight(86, 0.4)); //drive to end of line of cones, 99
+                addCommands(new Turn(60, 0.4)); //turn to line up with second gap
+                addCommands(new DriveStraight(38, 0.4)); //drive to tangent point with circle around last cone
+                addCommands(new Circle(-20, 300)); //drive circle around last cone
+                addCommands(new DriveStraight(40, 0.4)); //drive to tangent point with circle around last cone
+                addCommands(new Turn(60, 0.4)); //turn to line up with second gap
+                addCommands(new DriveStraight(99+40, 0.4)); //drive to end of line of cones
+                addCommands(new Turn(70,0.4));
+                addCommands(new DriveStraight(15,.4)); 
+                addCommands(new Circle(-28,-50));
                 break;
             case BOUNCE: //center halfway between B2 and D2
-                addCommand( new Circle(30,90));
-                addCommand(new DriveStraight(30,0.4));
-                addCommands(new Turn(-20,.4));
-                addCommands(new DriveStraight(-105, .4));
-                addCommands(new Circle(-30,160));
-                addCommands(new DriveStraight(-90,.4));
-                addCommand (new turn(-95));
-                addCommand(new drivetraight (-42.42, 0.4))
 
-        }
-        
-        //addCommands(new Turn(-28, 0.4)); //turn to line up with first gap
-                // addCommands(new DriveStraight(87, 0.4)); //drive through first gap
-                // addCommands(new Turn(28, 0.4)); //turn to be parallel with line of cones
-                // addCommands(new DriveStraight(120, 0.4)); //drive to end of line of cones
-                // addCommands(new Turn(28, 0.4)); //turn to line up with second gap
-                // addCommands(new DriveStraight(33.98, 0.4)); //drive to tangent point with circle around last cone
-                // addCommands(new Circle(30, 360)); //drive circle around last cone
-                // addCommands(new DriveStraight(33.98, 0.4)); //drive from circle to start of line of cones
-                // addCommands(new Turn (28, 0.4)); //turn to be parallel with line of cones
-                // addCommands(new DriveStraight(120, 0.4)); //drive to end of line of cones
-                // addCommands(new Turn(28, 0.4)); //turn to line up with end position
-                // addCommands(new DriveStraight(87, 0.4)); //drive to end position
+                addCommands(new DriveStraight(3,.4));
+                addCommands(new Circle(-28,90));
+                addCommands(new DriveStraight(-30,.4));
+                addCommands(new Turn(-20,.4));
+                addCommands(new DriveStraight(-55, .4));
+                addCommands(new Turn(180,0.6));
+                addCommands(new Circle(-30,160));
+                addCommands(new DriveStraight(90,.4));
+            }
     }
 }
