bb3a7c488a0f8231c97cf6d683704759ae3173d2
WAaron-500 <61605186+WAaron-500@users.noreply.github.com>
2021-05-12 03:53:39 -0400

    oops forgot something

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,7 +28,7 @@ public class Robot extends TimedRobot {
         System.out.println("Initializing" + RobotMap.botName);
 
         //Choose from ControllerTank, ControllerTankFO (Field Oriented extras), JoystickTank, ControllerSplitArcade, ControllerSplitCurvature, and Wii
-        UIMap.setDriveMode(UIMap.DriveMode.ControllerTank);
+        UIMap.setDriveMode(UIMap.DriveMode.MecanumDrive);
         System.out.println("Initializing" + UIMap.driveMode);
 
         //drive settings
